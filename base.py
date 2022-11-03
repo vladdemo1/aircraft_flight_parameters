5d52ad5644ffe4fdbeedc6487b4be212be995bce
vladdemo1 <v.demchenko@student.csn.khai.edu>
2022-11-03 12:49:04 +0200

    Completed all tasks about search fly info

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 """This file created for working with navigation data"""
 
 from datetime import timedelta
+from math import sin, cos, atan, sqrt
 
 # For example
 # 0GPGGA,060355.00,4950.4828,N,03638.5901,E,1,05,4.3,144.5,M,16.2,M,,*67
@@ -8,10 +9,12 @@ from datetime import timedelta
 
 # 0GPGGA     - command about write current message
 # 060355.00  - 06 hours 03 minutes 55.00 seconds
+
 # 4950.4828  - latitude: 49 degrees 50.4828 minutes
 # N          - Nord
 # 03638.5901 - longitude: 036 degrees 38.5901 minutes
 # E          - East
+
 # 1    - trash
 # 05   - trash
 # 4.3  - trash
@@ -23,7 +26,14 @@ from datetime import timedelta
 from message import MESSAGE
 
 
+EARTH_RADIUS_IN_KM = 6372.795
+EARTH_RADIUS_IN_M = 6372795
+
+
 class Aircraft:
+    """
+    Main class aircraft with data on fly
+    """
 
     def __init__(self, message) -> None:
         self._message = message
@@ -38,6 +48,9 @@ class Aircraft:
         self.search_data_for_lists()
 
     def search_data_for_lists(self) -> None:
+        """
+        Get all elements for data flight
+        """
         for element in self._message:
             info = element.split(',')
 
@@ -53,6 +66,9 @@ class Aircraft:
 
     @staticmethod
     def local_append(local_attr: list, value_to_append: str) -> None:
+        """
+        Append value to current list
+        """
         local_attr.append(value_to_append)
         return None
 
@@ -77,6 +93,9 @@ class Aircraft:
 
     @staticmethod
     def parser_time(str_with_time: str) -> tuple:
+        """
+        Get time info from string
+        """
         hours = int(str_with_time[:2])
         minutes = int(str_with_time[2:4])
         seconds = float(str_with_time[4:])
@@ -99,9 +118,125 @@ class Aircraft:
 
         return time_in_fly
 
+    def get_sum_flight_distance(self):
+        """
+        Get current summa all flight distance
+        """
+        sum_flight_distance = 0
 
-aircraft = Aircraft(message=MESSAGE)
+        for distance in self.get_list_distances_per_signal():
+            sum_flight_distance += distance
 
-# print(aircraft.get_max_hight_value())
-# print(aircraft.get_min_hight_value())
-aircraft.get_all_time_fly()
+        return sum_flight_distance
+
+    def get_list_distances_per_signal(self):
+        """
+        Get list with elements, where on one signal -> one distance
+        """
+        list_of_distances = []
+
+        fo_1_rad = 0
+        la_1_rad = 0
+
+        fo_2_rad = 0
+        la_2_rad = 0
+
+        for index in range(len(self._list_latitude)):
+            if index >= 19:
+                break
+
+            fo_1_rad = self.get_radians(self._list_latitude[index])
+            la_1_rad = self.get_radians(self._list_longtitude[index][1:])
+            
+            fo_2_rad = self.get_radians(self._list_latitude[index + 1])
+            la_2_rad = self.get_radians(self._list_longtitude[index+ 1][1:])
+
+            list_of_distances.append(self.get_distance_in_meters(fo_1_rad, la_1_rad, fo_2_rad, la_2_rad))
+        return list_of_distances
+
+
+    @staticmethod
+    def get_radians(value):
+        """
+        Get radians value from degrees and minutes
+        """
+        degrees: float = float(value[:2])
+        minutes: float = float(value[2:])
+        radians = degrees + (minutes / 60)
+        return radians
+
+    @staticmethod
+    def get_distance_in_meters(rad_fo_1, rad_la_1, rad_fo_2, rad_la_2):
+        """
+        Get value of distance by formula
+        """
+        delta_fo = rad_fo_2 - rad_fo_1
+        delta_la = rad_la_2 - rad_la_1
+
+        delta_ce_de = atan(sqrt((cos(rad_fo_2)*sin(delta_la))**2 + (cos(rad_fo_1)*sin(rad_fo_2) - sin(rad_fo_1)*cos(rad_fo_2)*cos(delta_la))**2)/((sin(rad_fo_1)*sin(rad_fo_2) + cos(rad_fo_1)*cos(rad_fo_2)*cos(delta_la))))
+
+        distance = EARTH_RADIUS_IN_KM * delta_ce_de
+        return distance * 1000
+
+    def get_middle_speed(self):
+        """
+        Get flight middle speed by all time in fly
+        """
+        all_distance = int(self.get_sum_flight_distance())
+        time_in_second = int(self.get_all_time_fly().seconds)
+
+        return all_distance / time_in_second
+
+    def get_delta_in_signals(self):
+        """
+        Get delta in seconds by all distans for seconds
+        """
+        all_time_in_sec = int(self.get_all_time_fly().seconds)
+        count_signals = int(len(self._list_commands))
+        return count_signals / all_time_in_sec
+
+    def get_min_max_speed_fly(self):
+        """
+        Get dict with minimal and maximum speed on fly
+        """
+        delta_signal_in_second = self.get_delta_in_signals()
+
+        speed_info = {
+            "min": 1000.0,
+            "max": 0.0,
+        }
+        for distance in self.get_list_distances_per_signal():
+            current_speed = distance / delta_signal_in_second
+            if current_speed > speed_info["max"]:
+                speed_info["max"] = current_speed
+            
+            if current_speed < speed_info["min"]:
+                speed_info["min"] = current_speed
+        
+        return speed_info
+        
+
+def base():
+    """
+    Base method for show all data info
+    """
+
+    aircraft = Aircraft(message=MESSAGE)
+
+    print(f"Сумарна довжина маршруту польоту: {round(aircraft.get_sum_flight_distance(), 2)} метрів.")
+
+    print(f"Середня швидкість польоту: {round(aircraft.get_middle_speed(), 2)} м/c.")
+
+    print(f"Мінімальна швидкість польоту: {round(aircraft.get_min_max_speed_fly()['min'], 2)} м/c.")
+
+    print(f"Максимальна швидкість польоту: {round(aircraft.get_min_max_speed_fly()['max'], 2)} м/c.")
+
+    print(f"Мінімальна висота польоту: {round(aircraft.get_min_hight_value(), 2)} метрів.")
+
+    print(f"Максимальна висота польоту: {round(aircraft.get_max_hight_value(), 2)} метрів.")
+    
+    print(f"Загальний час усього польоту: {aircraft.get_all_time_fly()}.")
+    
+
+if __name__ == "__main__":
+    base()
